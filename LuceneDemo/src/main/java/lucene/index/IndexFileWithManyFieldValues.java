21368fc28dd86a9ca515009450297aded16df964
LuXugang <q133662517@gmail.com>
2019-06-21 00:25:15 +0800

    同步demo
    同步demo

--- LuceneDemo/src/main/java/lucene/index/IndexFileWithManyFieldValues.java
+++ LuceneDemo/src/main/java/lucene/index/IndexFileWithManyFieldValues.java
@@ -23,10 +23,13 @@ public class IndexFileWithManyFieldValues {
   private Directory directory ;
   private Directory directory2;
   private Directory directory3;
+  private Analyzer analyzer = new WhitespaceAnalyzer();
+  private IndexWriterConfig conf = new IndexWriterConfig(analyzer);
+  private IndexWriter indexWriter;
 
   {
     try {
-//      FileOperation.deleteFile("./data");
+      FileOperation.deleteFile("./data");
 //      FileOperation.deleteFile("./data1");
 //      directory3 = FSDirectory.open(Paths.get("./data01"));
 //      directory2 = FSDirectory.open(Paths.get("./data02"));
@@ -37,15 +40,19 @@ public class IndexFileWithManyFieldValues {
 //      primaryExtensions.add("nvm");
 //      directory = new FileSwitchDirectory(primaryExtensions, directory3, directory2, true);
       directory = FSDirectory.open(Paths.get("./data"));
+      conf.setUseCompoundFile(true);
+      conf.setMergePolicy(NoMergePolicy.INSTANCE);
+      conf.setIndexDeletionPolicy(NoDeletionPolicy.INSTANCE);
+      conf.setSoftDeletesField("myDeleteFiled");
+      conf.setRAMBufferSizeMB(10);
+      indexWriter = new IndexWriter(directory, conf);
 //      directory = new NIOFSDirectory(Paths.get("./data"));
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  private Analyzer analyzer = new WhitespaceAnalyzer();
-  private IndexWriterConfig conf = new IndexWriterConfig(analyzer);
-  private IndexWriter indexWriter;
+
 
   public void doIndex() throws Exception {
 
@@ -58,55 +65,61 @@ public class IndexFileWithManyFieldValues {
     type.setTokenized(true);
     type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS);
 
-    conf.setUseCompoundFile(true);
-    conf.setMergePolicy(NoMergePolicy.INSTANCE);
-    conf.setIndexDeletionPolicy(NoDeletionPolicy.INSTANCE);
-    conf.setSoftDeletesField("myDeleteFiled");
-    indexWriter = new IndexWriter(directory, conf);
+
 
     int count = 0;
     int n = 0;
     Document doc ;
-    while (count++ < 0) {
+    while (count++ < 100000000) {
 //      doc.add(new Field("content", "abc", type));
 //      doc.add(new Field("content", "cd", type));
 //      doc.add(new StoredField("content", 3));
 //      doc.add(new Field("author", "efg", type));
 
       // 文档0
-      doc = new Document();
-      doc.add(new NumericDocValuesField("abc", 0));
-      doc.add(new Field("content", "a", type));
-      doc.add(new IntPoint("abc", 3, 5, 9));
-      indexWriter.addDocument(doc);
+//      doc = new Document();
+//      doc.add(new NumericDocValuesField("abc", 0));
+//      doc.add(new Field("content", "a", type));
+//      doc.add(new IntPoint("abc", 3, 5, 9));
+//      indexWriter.addDocument(doc);
 
       // 文档1
-      doc = new Document();
-      doc.add(new SortedDocValuesField("forSort", new BytesRef("a")));
-      doc.add(new Field("content", "b", type));
-      doc.add(new IntPoint("abc", 3, 9, 9));
-      indexWriter.addDocument(doc);
+//      doc = new Document();
+//      doc.add(new SortedDocValuesField("forSort", new BytesRef("a")));
+//      doc.add(new Field("content", "b", type));
+//      doc.add(new IntPoint("abc", 3, 9, 9));
+//      indexWriter.addDocument(doc);
 
       // 文档2
-      doc = new Document();
-      doc.add(new NumericDocValuesField("abc", 0));
-      doc.add(new Field("content", "c", type));
-      indexWriter.addDocument(doc);
+//      doc = new Document();
+//      doc.add(new NumericDocValuesField("abc", 0));
+//      doc.add(new Field("content", "c", type));
+//      indexWriter.addDocument(doc);
 
+      indexWriter.deleteDocuments(new Term("content", "a"));
+      indexWriter.deleteDocuments(new Term("content", "b"));
+      indexWriter.deleteDocuments(new Term("content", getRandomValue()));
+      indexWriter.deleteDocuments(new Term("content", getRandomValue()));
+      indexWriter.deleteDocuments(new Term("content", getRandomValue()));
+      indexWriter.deleteDocuments(new Term("content", getRandomValue()));
+      indexWriter.deleteDocuments(new Term("content", getRandomValue()));
+      indexWriter.deleteDocuments(new Term("content", getRandomValue()));
+      indexWriter.deleteDocuments(new Term("content", getRandomValue()));
+      indexWriter.deleteDocuments(new Term("content", getRandomValue()));
 //      indexWriter.updateDocValues(new Term("content", "c"), new NumericDocValuesField("文档2", 3));
 //      indexWriter.updateDocValues(new Term("content", "a"), new NumericDocValuesField("文档0", 4));
 
       // 文档3
-      doc = new Document();
-      doc.add(new Field("content", "d", type));
-      doc.add(new BinaryDocValuesField("myDocValues", new BytesRef("d")));
-      indexWriter.addDocument(doc);
+//      doc = new Document();
+//      doc.add(new Field("content", "d", type));
+//      doc.add(new BinaryDocValuesField("myDocValues", new BytesRef("d")));
+//      indexWriter.addDocument(doc);
 //      indexWriter.flush();
 //      indexWriter.updateDocValues(new Term("content", "d"), new BinaryDocValuesField("文档3", new BytesRef("e")));
 
-     if(count % 10 == 0){
-       indexWriter.commit();
-     }
+//     if(count % 10 == 0){
+//       indexWriter.commit();
+//     }
 
     }
     Map<String, String> userData = new HashMap<>();
@@ -123,7 +136,7 @@ public class IndexFileWithManyFieldValues {
     ScoreDoc[] scoreDocs = searcher.search(query, 10).scoreDocs;
 
     Document document  = reader.document(2);
-    System.out.println(document.get("content"));
+    System.out.println("DONE");
 
     // Per-top-reader state:
   }
@@ -184,8 +197,41 @@ public class IndexFileWithManyFieldValues {
     return stringBuilder.toString();
   }
 
+  public static class MyThread implements Runnable{
+    IndexFileWithManyFieldValues test;
+
+    public MyThread(IndexFileWithManyFieldValues test) {
+      this.test = test;
+    }
+
+    @Override
+    public void run(){
+      try {
+        test.doIndex();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
   public static void main(String[] args) throws Exception{
     IndexFileWithManyFieldValues test = new IndexFileWithManyFieldValues();
-    test.doIndex();
+
+    MyThread a = new MyThread(test);
+    MyThread b = new MyThread(test);
+
+    Thread t1 = new Thread(a, "ThreadAAAA");
+    Thread t2 = new Thread(b, "ThreadBBBB");
+
+    System.out.println("a start to run");
+    t1.setDaemon(true);
+    System.out.println(""+t1.isDaemon()+"");
+    t1.start();
+    System.out.println("b start to run");
+    t2.setDaemon(true);
+    System.out.println(""+t2.isDaemon()+"");
+//    t2.run();
+    t1.join();
+//    t2.join();
   }
 }
