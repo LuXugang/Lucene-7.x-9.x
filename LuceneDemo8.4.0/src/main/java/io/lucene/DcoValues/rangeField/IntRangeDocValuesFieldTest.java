a5119200c0fc12fd8b511626b1bdcc36a89e39b0
luxugang <xugang.lu@eoitek.com>
2020-07-23 01:09:31 +0800

    demo同步
    demo同步

--- LuceneDemo8.4.0/src/main/java/io/lucene/DcoValues/rangeField/IntRangeDocValuesFieldTest.java
+++ LuceneDemo8.4.0/src/main/java/io/lucene/DcoValues/rangeField/IntRangeDocValuesFieldTest.java
@@ -11,6 +11,7 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.NoMergePolicy;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.MMapDirectory;
@@ -32,8 +33,8 @@ public class IntRangeDocValuesFieldTest {
             e.printStackTrace();
         }
     }
-    private Analyzer analyzer = new WhitespaceAnalyzer();
-    private IndexWriterConfig conf = new IndexWriterConfig(analyzer);
+    private final Analyzer analyzer = new WhitespaceAnalyzer();
+    private final IndexWriterConfig conf = new IndexWriterConfig(analyzer);
     private IndexWriter indexWriter;
 
     public void doIndexAndSearch() throws Exception {
@@ -43,22 +44,42 @@ public class IntRangeDocValuesFieldTest {
         Document doc;
         // 文档0
         doc = new Document();
-        int[] min1 = {1, 3};
-        int[] max1 = {2, 4};
+        int[] min1 = {1, 1};
+        int[] max1 = {4, 4};
+        // 第一个RangeField
         doc.add(new IntRangeDocValuesField("level", min1, max1));
         indexWriter.addDocument(doc);
         // 文档1
         doc = new Document();
-        int[] min2 = {3, 5};
-        int[] max2 = {4, 6};
+        int[] min2 = {3, 2};
+        int[] max2 = {6, 5};
+        // 第二个RangeField
         doc.add(new IntRangeDocValuesField("level", min2, max2));
+        indexWriter.addDocument(doc);
+        // 文档2
+        doc = new Document();
+        int[] min3 = {8, 8};
+        int[] max3 = {10, 10};
+        // 第三个RangeField
+        doc.add(new IntRangeDocValuesField("level", min3, max3));
+        indexWriter.addDocument(doc);
+        // 文档3
+        doc = new Document();
+        int[] min4 = {9, 6};
+        int[] max4 = {10, 7};
+        // 第四个RangeField
+        doc.add(new IntRangeDocValuesField("level", min4, max4));
         indexWriter.addDocument(doc);
         DirectoryReader reader = DirectoryReader.open(indexWriter);
         IndexSearcher searcher = new IndexSearcher(reader);
-        final int[] lowRange = {0, 4};
-        final int[] highRange = {2, 8};
-        Query query = IntRangeDocValuesField.newSlowIntersectsQuery("level", lowRange, highRange);
-        TopDocs docs2 = searcher.search(query, 1000);
+        final int[] lowQueryRange = {2, 0};
+        final int[] highQueryRange = {8, 8};
+        Query query = IntRangeDocValuesField.newSlowIntersectsQuery("level", lowQueryRange, highQueryRange);
+        ScoreDoc[] scoreDocs = searcher.search(query, 1000).scoreDocs;
+        assert scoreDocs.length == 3;
+        assert scoreDocs[0].doc == 0;
+        assert scoreDocs[1].doc == 1;
+        assert scoreDocs[2].doc == 2;
     }
 
     public static void main(String[] args) throws Exception{
