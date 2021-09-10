606592ddea37f24086e770cded44ed3701d657c2
Lu Xugang <q133662517@Gmail.com>
2021-09-10 13:20:30 +0800

    更新demo介绍段内排序IndexSort
    更新demo介绍段内排序IndexSort

--- LuceneDemo8.9.0/src/main/java/index/IndexSortTest.java
+++ LuceneDemo8.9.0/src/main/java/index/IndexSortTest.java
@@ -16,6 +16,9 @@ import util.FileOperation;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+/**
+ * 文章中的demo 不要修改
+ */
 public class IndexSortTest {
     private Directory directory;
 
@@ -29,55 +32,66 @@ public class IndexSortTest {
     }
 
     private Analyzer analyzer = new WhitespaceAnalyzer();
-    private IndexWriterConfig conf = new IndexWriterConfig(analyzer);
-    private IndexWriter indexWriter;
 
     public void doSearch() throws Exception {
         FieldType fieldType = new FieldType();
         fieldType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS);
         fieldType.setStored(true);
         fieldType.setTokenized(true);
-        SortedSetSortField sortField = new SortedSetSortField("sort1", true);
-        Sort sort = new Sort(sortField);
-        conf.setIndexSort(sort);
-        conf.setUseCompoundFile(false);
-        conf.setMergeScheduler(new SerialMergeScheduler());
-        indexWriter = new IndexWriter(directory, conf);
         Document doc;
+
+        IndexWriterConfig conf = new IndexWriterConfig(analyzer);
+        SortedSetSortField sortField1 = new SortedSetSortField("sort0", true, SortedSetSelector.Type.MIN);
+        SortedSetSortField sortField2 = new SortedSetSortField("sort1", true, SortedSetSelector.Type.MAX);
+        SortField[] sortFields = new SortField[2];
+        sortFields[0] = sortField1;
+        sortFields[1] = sortField2;
+        Sort sort = new Sort(sortFields);
+        conf.setIndexSort(sort);
+        IndexWriter indexWriter = new IndexWriter(directory, conf);
 
         // 文档0
         doc = new Document();
-        doc.add(new Field("title", "i love china china", fieldType));
-        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("c")));
+        doc.add(new Field("sequence", "第0篇添加的文档", fieldType));
+        doc.add(new SortedSetDocValuesField("sort0", new BytesRef("c1")));
+        doc.add(new SortedSetDocValuesField("sort0", new BytesRef("c2")));
+        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("a1")));
+        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("a2")));
         indexWriter.addDocument(doc);
         // 文档1
         doc = new Document();
-        doc.add(new Field("body", "china", fieldType));
-        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("a")));
+        doc.add(new Field("sequence", "第1篇添加的文档", fieldType));
+        doc.add(new SortedSetDocValuesField("sort0", new BytesRef("b1")));
+        doc.add(new SortedSetDocValuesField("sort0", new BytesRef("c1")));
+        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("e1")));
+        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("e2")));
         indexWriter.addDocument(doc);
         // 文档2
         doc = new Document();
-        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("e")));
-        doc.add(new Field("title", "i love china china", fieldType));
-        doc.add(new Field("body", "china", fieldType));
+        doc.add(new Field("sequence", "第2篇添加的文档", fieldType));
+        doc.add(new SortedSetDocValuesField("sort0", new BytesRef("b1")));
+        doc.add(new SortedSetDocValuesField("sort0", new BytesRef("b2")));
+        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("e2")));
+        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("f2")));
         indexWriter.addDocument(doc);
         // 文档3
         doc = new Document();
-        doc.add(new Field("body", "china", fieldType));
-        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("b")));
+        doc.add(new Field("sequence", "第3篇添加的文档", fieldType));
+        doc.add(new SortedSetDocValuesField("sort0", new BytesRef("b1")));
+        doc.add(new SortedSetDocValuesField("sort0", new BytesRef("b2")));
+        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("e1")));
+        doc.add(new SortedSetDocValuesField("sort1", new BytesRef("e2")));
         indexWriter.addDocument(doc);
-
+        // 文档4
+        doc = new Document();
+        doc.add(new Field("sequence", "第4篇添加的文档", fieldType));
+        indexWriter.addDocument(doc);
         indexWriter.commit();
-
-        Query query = new TermQuery(new Term("title", new BytesRef("china")));
-
-        DirectoryReader directoryReader = DirectoryReader.open(indexWriter);
-        IndexSearcher searcher = new IndexSearcher(directoryReader);
-
-        ScoreDoc[] result = searcher.search(query, 100).scoreDocs;
-//        ScoreDoc[] result = searcher.search(booleanQuery, 100).scoreDocs;
+        DirectoryReader reader= DirectoryReader.open(indexWriter);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        ScoreDoc[] result = searcher.search(new MatchAllDocsQuery(), 100).scoreDocs;
         for (ScoreDoc scoreDoc : result) {
-            System.out.println("文档号: "+scoreDoc.doc+" 文档分数: "+scoreDoc.score+"");
+            System.out.println("段内排序后的文档号: "+scoreDoc.doc+" VS 段内排序前的文档: "+reader.document(scoreDoc.doc).get("sequence")+"");
         }
 
         System.out.println("DONE");
