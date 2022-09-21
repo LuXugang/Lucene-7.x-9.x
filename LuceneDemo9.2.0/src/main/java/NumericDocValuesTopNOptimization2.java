4e42b6c0e999de8b367df7fed738f028a7c180cd
Lu Xugang <luxugang@apache.org>
2022-09-21 14:05:51 +0800

    spotless
    spotless

--- LuceneDemo9.2.0/src/main/java/NumericDocValuesTopNOptimization2.java
+++ LuceneDemo9.2.0/src/main/java/NumericDocValuesTopNOptimization2.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Random;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -16,15 +19,8 @@ import org.apache.lucene.search.TopFieldCollector;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.MMapDirectory;
 
-import java.io.File;
-import java.nio.file.Paths;
-import java.util.Random;
-
-/**
- * example for https://github.com/apache/lucene/issues/11773 , do not modifier the code
- */
+/** example for https://github.com/apache/lucene/issues/11773 , do not modifier the code */
 public class NumericDocValuesTopNOptimization2 {
-
 
     public void doSearch() throws Exception {
         Directory directory;
@@ -46,19 +42,20 @@ public class NumericDocValuesTopNOptimization2 {
         // index 80000 documents in boundary should be matched by PointRangeQuery
         while (count++ < 80000) {
             sortValue = random.nextLong();
-            if(sortValue <= lowerValue){
+            if (sortValue <= lowerValue) {
                 // make sure the sortValue is in the query boundary
-                sortValue =  20L;
+                sortValue = 20L;
             }
             indexWriter.addDocument(addDocument(sortValue, "sortedField"));
         }
 
         // index 10w documents out boundary
         int outOfBoundaryDocumentCount = random.nextBoolean() ? 10 : 20000;
+        outOfBoundaryDocumentCount = 20000;
         System.out.println("outOfBoundaryDocumentCount:" + outOfBoundaryDocumentCount);
         count = 0;
         while (count++ < outOfBoundaryDocumentCount) {
-            sortValue = random.nextInt((int)lowerValue - 1);
+            sortValue = random.nextInt((int) lowerValue - 1);
             indexWriter.addDocument(addDocument(sortValue, "sortedField"));
         }
 
@@ -68,19 +65,18 @@ public class NumericDocValuesTopNOptimization2 {
         IndexSearcher searcher = new IndexSearcher(reader);
         SortField sortField = new SortedNumericSortField("sortedField", SortField.Type.LONG);
         sortField.setMissingValue(Long.MAX_VALUE);
-        Query rangeQuery =
-                LongPoint.newRangeQuery(
-                        "sortedField", lowerValue, upperValue);
+        Query rangeQuery = LongPoint.newRangeQuery("sortedField", lowerValue, upperValue);
         Sort sort = new Sort(sortField);
         int topN = 3;
         TopFieldCollector collector = TopFieldCollector.create(sort, topN, 1000);
         searcher.search(rangeQuery, collector);
         System.out.println("hits in collector: " + collector.getTotalHits() + "");
-        indexWriter.close();;
+        indexWriter.close();
+        ;
         reader.close();
     }
 
-    private Document addDocument(Long value, String fieldName){
+    private Document addDocument(Long value, String fieldName) {
         Document doc = new Document();
         doc.add(new StringField("content", String.valueOf(value), Field.Store.YES));
         doc.add(new NumericDocValuesField(fieldName, value));
