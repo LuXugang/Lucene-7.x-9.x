b81cad290982533c2d226edfc03923015d5f860f
Lu Xugang <luxugang@apache.org>
2023-05-25 17:41:59 +0800

    backupup

--- LuceneDemo9.6.0/src/main/java/TestTieredMergePolicy.java
+++ LuceneDemo9.6.0/src/main/java/TestTieredMergePolicy.java
@@ -2,18 +2,20 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.SortedSetDocValuesField;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.NoMergePolicy;
 import org.apache.lucene.index.SerialMergeScheduler;
-import org.apache.lucene.index.SortedSetDocValues;
-import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.MMapDirectory;
 import org.apache.lucene.util.BytesRef;
-import org.apache.lucene.util.VectorUtil;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -38,35 +40,42 @@ public class TestTieredMergePolicy {
     public void doSearch() throws Exception {
         conf.setUseCompoundFile(false);
         indexWriter = new IndexWriter(directory, conf);
+        conf.setMergeScheduler(new SerialMergeScheduler());
+//        conf.setMergePolicy(NoMergePolicy.INSTANCE);
         Random random = new Random();
         Document doc;
         //
+        int segmentSize = 11;
+        int segmentCount = 0;
         int count = 0;
+        while (segmentCount++ < segmentSize){
+            count = 0;
+            while (count++ != 2000){
+                doc = new Document();
+                doc.add(new SortedSetDocValuesField("sortedSet", new BytesRef("a")));
+                doc.add(new StringField("name", String.valueOf(random.nextInt(1000000)), StringField.Store.YES));
+                indexWriter.addDocument(doc);
+            }
+            indexWriter.commit();
+        }
 
-        doc = new Document();
-        doc.add(new SortedSetDocValuesField("sortedSet", new BytesRef("a")));
-        doc.add(new SortedSetDocValuesField("sortedSet", new BytesRef("a")));
-        doc.add(new SortedSetDocValuesField("sortedSet", new BytesRef("b")));
-        indexWriter.addDocument(doc);
-
-        doc = new Document();
-        doc.add(new SortedSetDocValuesField("sortedSet", new BytesRef("b")));
-        doc.add(new SortedSetDocValuesField("sortedSet", new BytesRef("b")));
-        indexWriter.addDocument(doc);
-
+//        indexWriter.forceMerge(1);
+//
         DirectoryReader reader = DirectoryReader.open(indexWriter);
         IndexSearcher searcher = new IndexSearcher(reader);
 
-        System.out.println("DONE");
-    }
-
-    private float[] randomVector(int dim, Random random) {
-        float[] v = new float[dim];
-        for (int i = 0; i < dim; i++) {
-            v[i] = random.nextFloat();
+        Query query = new MatchAllDocsQuery();
+        System.out.println("段的数量: "+reader.leaves().size());
+        for (LeafReaderContext leaf : reader.leaves()) {
+            System.out.println("段: "+leaf.toString()+"");
         }
-        VectorUtil.l2normalize(v);
-        return v;
+        System.out.println(reader.maxDoc());
+        ScoreDoc[] scoreDocs = searcher.search(query, 2000000).scoreDocs;
+        for (ScoreDoc scoreDoc : scoreDocs) {
+
+        }
+
+        System.out.println("DONE");
     }
 
     public static void main(String[] args) throws Exception {
