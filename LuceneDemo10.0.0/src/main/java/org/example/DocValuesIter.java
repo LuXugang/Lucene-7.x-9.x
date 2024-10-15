b4c9a3abef7527c1a4a103a97ef91c6c2610dbed
Lu Xugang <luxugang@apache.org>
2024-10-16 00:37:46 +0800

    新增Lucene10.0.0Demo
    新增Lucene10.0.0Demo

--- LuceneDemo10.0.0/src/main/java/org/example/DocValuesIter.java
+++ LuceneDemo10.0.0/src/main/java/org/example/DocValuesIter.java
@@ -3,15 +3,13 @@ package org.example;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.IntField;
+import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.IntPoint;
-import org.apache.lucene.document.NumericDocValuesField;
 import org.apache.lucene.document.SortedNumericDocValuesField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.SortedNumericDocValues;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
@@ -29,7 +27,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Random;
 
-public class TestPointRangeQuery {
+public class DocValuesIter {
     private Directory directory;
 
     {
@@ -57,41 +55,36 @@ public class TestPointRangeQuery {
         // 0
         doc = new Document();
         doc.add(new IntPoint("content", 1));
-        doc.add(new SortedNumericDocValuesField("content", 2));
+        doc.add(SortedNumericDocValuesField.indexedField("content", 2));
         doc.add(new StringField("content", "a", org.apache.lucene.document.Field.Store.YES));
         doc.add(new StringField("title", "文档0", org.apache.lucene.document.Field.Store.YES));
         indexWriter.addDocument(doc);
         // 1
         doc = new Document();
         doc.add(new IntPoint("content", 10));
-        doc.add(new SortedNumericDocValuesField("content", 3));
+        doc.add(SortedNumericDocValuesField.indexedField("content", 100));
         doc.add(new StringField("content", "a", org.apache.lucene.document.Field.Store.YES));
         doc.add(new StringField("title", "文档1", org.apache.lucene.document.Field.Store.YES));
         indexWriter.addDocument(doc);
         // 2
         doc = new Document();
         doc.add(new IntPoint("content", 10));
-        doc.add(new SortedNumericDocValuesField("content", 1));
+        doc.add(SortedNumericDocValuesField.indexedField("content", 1));
         doc.add(new StringField("content", "a", org.apache.lucene.document.Field.Store.YES));
         doc.add(new StringField("title", "文档2", org.apache.lucene.document.Field.Store.YES));
         indexWriter.addDocument(doc);
 
-
-        doc = new Document();
-//    doc.add(new IntPoint("title", 10, 55));
-//        indexWriter.addDocument(doc);
-
-//        int count = 0 ;
-//        while (count++ < (2048 * 2)){
-//            doc = new Document();
-//            int a = random.nextInt(100);
-//            a = a == 0 ? a + 1 : a;
-//            int b = random.nextInt(100);
-//            b = b == 0 ? b + 1 : b;
-//            doc.add(new IntPoint("content", a));
-//            doc.add(new SortedNumericDocValuesField("content", a));
-//            indexWriter.addDocument(doc);
-//        }
+        int count = 0 ;
+        while (count++ < (4096 * 17)){
+            doc = new Document();
+            int a = random.nextInt(100);
+            a = a < 2 ? a + 10 : a;
+            a = a > 90 ? a - 2 : a;
+            doc.add(new IntPoint("content", a));
+            doc.add(SortedNumericDocValuesField.indexedField("content", a));
+            doc.add(new StringField("content", "a", org.apache.lucene.document.Field.Store.YES));
+            indexWriter.addDocument(doc);
+        }
 
         indexWriter.commit();
         DirectoryReader r = DirectoryReader.open(indexWriter);
@@ -110,16 +103,7 @@ public class TestPointRangeQuery {
         TopFieldDocs docs = s.search(query, 10, sort);
         for (ScoreDoc scoreDoc : docs.scoreDocs) {
             System.out.println(scoreDoc.doc);
-            String titleValue = r.document(scoreDoc.doc).getField("title").toString();
-            System.out.println(titleValue);
         }
-        TopDocs topDocs = s.search(query, 10);
-        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-            System.out.println(scoreDoc.doc);
-            String titleValue = r.document(scoreDoc.doc).getField("title").toString();
-            System.out.println(titleValue);
-        }
-
 
         // Per-top-reader state:k
     }
@@ -127,7 +111,7 @@ public class TestPointRangeQuery {
 
 
     public static void main(String[] args) throws Exception{
-        TestPointRangeQuery query = new TestPointRangeQuery();
+        DocValuesIter query = new DocValuesIter();
         query.doIndex();
     }
 }
